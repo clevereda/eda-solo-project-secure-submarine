b0a484dc93d688b8eb7f4c90e5c0f95d4af7e3b7
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-01 16:17:42 -0500

    Hashing.

--- database.sql
+++ database.sql
@@ -10,21 +10,22 @@ CREATE TABLE "user" (
     "clearance_level" INTEGER NOT NULL DEFAULT 0
 );
 
-INSERT INTO "user" ("username", "password", "clearance_level")
-VALUES ('Admiral Greer', 'tuna', 18),
-('Captain Borodin', 'shark', 10),
-('Lieutenant Nguyen', 'fishy', 4),
-('Lieutenant Ryan', 'tuna', 4);
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "clearance_level" INTEGER NOT NULL DEFAULT 0
+);
 
 
-CREATE TABLE "secret" (
+CREATE TABLE "seqcret" (
     "id" SERIAL PRIMARY KEY,
     "content" VARCHAR (80) UNIQUE NOT NULL,
     "secrecy_level" INTEGER NOT NULL DEFAULT 0
 );
 
-INSERT INTO "secret" ("content", "secrecy_level")
-VALUES ('Admirals Only: Captain Borodin is totally weird.', 13),
-('Captains Or Above: Lieutenant Ryan is looking fly.', 6),
-('Lieutenants Or Above: We are heading to the Bahamas.', 3);
-
+INSERT INTO "user" ("username", "password", "clearance_level")
+VALUES ('Admiral Greer', '$2b$10$p5Wkte33hlOBOcUtJie6H.PnCvk8v.KjZspVoAFtT7g5v5xK.EXVG', 18),
+('Captain Borodin', '$2b$10$p5Wkte33hlOBOcUtJie6H.ZIgFjzr4zY8FItxC8gZyqIWD5gYmL0m', 10),
+('Lieutenant Nguyen', '$2b$10$p5Wkte33hlOBOcUtJie6H.vaUd5ikB1LWCbVZAA87BR63NiDorn1C', 4),
+('Lieutenant Ryan', '$2b$10$p5Wkte33hlOBOcUtJie6H.PnCvk8v.KjZspVoAFtT7g5v5xK.EXVG', 4);
