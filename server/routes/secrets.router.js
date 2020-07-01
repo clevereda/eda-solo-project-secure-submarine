afb4a22048ea138c0b1f7181d94d4cf630f9ba7b
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-01 16:03:46 -0500

    Only display secrets with the right security level

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,8 +5,11 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user:', req.user);
-  pool.query('SELECT * FROM "secret";')
-    .then(results => res.send(results.rows))
+  pool.query('SELECT * FROM "secret" WHERE secrecy_level<= $1;', [req.user.clearance_level])
+    .then(results => {
+      console.log(results.rows);
+      res.send(results.rows);
+    })
     .catch(error => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
